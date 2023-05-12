d47b799986a9738db4e03de1a1659f186223e1ec
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2023-05-12 18:05:23 +0700

    Salah sengaja

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,7 +6,7 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.print450("Count:" + counter.getCount());
 		
 		counter.increment();
 		
